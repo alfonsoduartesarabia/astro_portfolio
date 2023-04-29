7f6d4be38676b17b615b442b1c1ab4b9bc7e0ab2
Alfonso Duarte-Sarabia <alfonsoduarte97@gmail.com>
2023-04-29 01:59:47 -0700

    added tank game

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
+
+// import cloudflare from "@astrojs/cloudflare";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
