cc17849febe482c943bc3d8e67e7413adde240b3
Alfonso Duarte-Sarabia <alfonsoduarte97@gmail.com>
2023-05-02 18:43:22 -0700

    updated config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,12 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-// import cloudflare from "@astrojs/cloudflare";
-
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.alfonsoduarte.dev/',
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare()
 });
